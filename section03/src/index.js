475488212e710c32a1ca1b927d4e56adc58da1c9
Bumsoo <161221357+nawe1@users.noreply.github.com>
2025-01-08 15:44:50 +0900

    Node-React 기본 설정방법

---
+++ section03/src/index.js
@@ -0,0 +1,9 @@
+import mul, { add, sub } from "./math.js";
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+//console.log(add(1, 2));
+//console.log(sub(4, 2));
+//console.log(mul(2, 3));
